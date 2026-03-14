fbcd09fd3e83edff8a990985a79229e1df791e77
tenplus1 <tenplus1@users.noreply.github.com>
2026-03-14 10:28:52 +0000

    use core.
    use core.

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-local modpath = minetest.get_modpath("abritorch").. DIR_DELIM
+local modpath = core.get_modpath("abritorch").. DIR_DELIM
 
 abritorch = {}
 
-dofile(modpath.."torches.lua")
-dofile(modpath.."crafting.lua")
+dofile(modpath .. "torches.lua")
+dofile(modpath .. "crafting.lua")
 
 abritorch.init = {}
